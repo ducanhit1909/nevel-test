558db3c66c2c75fc1e0dc61f192e874693b819c1
Phan Duc Anh <anhphan190913@gmail.com>
2024-10-28 17:58:28 +0700

    rename file

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
 import Banner from "./components/Banner";
-import Footer from "./components/Footer";
 import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 const Home: React.FC = () => {
   return (
